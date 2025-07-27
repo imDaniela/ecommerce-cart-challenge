8ce2350963f801f8c979c4822ae12ed218d89b3a
imDaniela <danielavlealr@gmail.com>
2025-07-27 12:15:17 +0200

    Update OrdenItemDTO and assembler to return product name

--- src/Cart/Application/Assembler/OrdenItemAssembler.php
+++ src/Cart/Application/Assembler/OrdenItemAssembler.php
@@ -6,9 +6,17 @@ use App\Cart\Domain\Model\OrdenItem;
 use App\Cart\Domain\ValueObject\Cantidad;
 use App\Cart\Domain\ValueObject\OrdenId;
 use App\Cart\Domain\ValueObject\ProductoId;
+use App\Cart\Application\Repository\ProductRepository;
 
 class OrdenItemAssembler
 {
+    private ProductRepository $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     public function toDTO(OrdenItem $ordenItem): OrdenItemDTO
     {
         $dto = new OrdenItemDTO();
@@ -28,5 +36,21 @@ class OrdenItemAssembler
             new OrdenId($dto->id_orden),
             new Cantidad($dto->cantidad)
         );
+    }
+
+    public function fromEntity(OrdenItem $ordenItem): OrdenItemDTO
+    {
+        $idProducto = $ordenItem->getIdProducto()->getValue();
+        $producto = $this->productRepository->findById(new ProductoId($idProducto));
+        $nombreProducto = $producto ? $producto->getNombre()->getValue() : '';
+
+        $dto = new OrdenItemDTO();
+        $dto->id = $ordenItem->getId();
+        $dto->id_producto = $idProducto;
+        $dto->nombre_producto = $nombreProducto;
+        $dto->id_orden = $ordenItem->getIdOrden()->getValue();
+        $dto->cantidad = $ordenItem->getCantidad()->getValue();
+
+        return $dto;
     }
 }
--- src/Cart/Application/DTO/OrdenItemDTO.php
+++ src/Cart/Application/DTO/OrdenItemDTO.php
@@ -1,10 +1,23 @@
 <?php   
 namespace App\Cart\Application\DTO;
+use App\Cart\Domain\Model\OrdenItem;
 
-class OrdenItemDTO
+class OrdenItemDTO implements \JsonSerializable
 {
     public $id;
     public $id_producto;
+    public $nombre_producto;
     public $id_orden;
     public $cantidad;
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'id_producto' => $this->id_producto,
+            'nombre_producto' => $this->nombre_producto,
+            'id_orden' => $this->id_orden,
+            'cantidad' => $this->cantidad,
+        ];
+    }
 }
